87d80de980ebbec5b8be18f0d7e12cfb437c8b46
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-09-19 19:48:10 +0000

    styled scorebaord component

--- client/ui/Scoreboard.vue
+++ client/ui/Scoreboard.vue
@@ -1,21 +1,21 @@
 <template>
     <div class='col-4 col-offset-8'>
         <div class='row'>
-                <span class='name-placard' id='player-placard'>YOU</span>
+                <span class='placard' id='player-placard'>YOU</span>
             </div>
             <div class='row'>
                 <div class='health-meter col-10 col-offset-1' id='player-health-meter'></div>
             </div>
             
         <div class='row'>
-                <span class='name-placard' id='opponent-placard'>opponent</span>
+                <span class='placard' id='opponent-placard'>OPPONENT</span>
             </div>
             <div class='row'>
                 <div class='health-meter col-10 col-offset-1' id='opponent-health-meter'></div>
         </div>
         
         <div class='row'>
-            <span class='question-count'>Questions: </span>
+            <span class='question-count'>Questions: {{questionCount}}</span>
         </div>
         
     </div>
@@ -23,7 +23,14 @@
 </template>
 <script>
 export default {
-}
+    data() {
+        return {
+            
+            questionCount: 0,
+            
+        };
+    }
+};
     
 </script>
 <style>
--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -2,6 +2,7 @@
     <div>
         <h1>Trivia Rumble</h1>
         <welcome></welcome>
+        <scoreboard class='scoreboard'></scoreboard>
         
     </div>
 </template>
