4773aaf8d1a00851780154042bfc159e54cee8ec
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-09-20 04:29:09 +0000

    added show rules feature

--- client/ui/Welcome.vue
+++ client/ui/Welcome.vue
@@ -5,11 +5,26 @@
             <button class='btn' @click='showRules = !showRules'>Learn How To Play</button>
             <button class='btn' @click='newGame'>Challenge an Opponent</button>
         </div>
+        <div class='rules-card' v-if='showRules' @click='showRules = !showRules'>
+            <span class='close-card'>X</span>
+            <p>Create your fighter. Tell us his attributes. Give your fighter a name,
+            and list the fighter's attributes.</p>
+            <p>Challenge an Opponent of your desired skill level. The higher the skill level of 
+            your opponents, the quicker you will be able to climb the rankings.</p>
+        </div>
     </div>
     
     
 </template>
 <script>
+export default {
+    data() {
+        return {
+            showRules: false,
+            
+        };
+    }
+};
     
 </script>
 <style>
