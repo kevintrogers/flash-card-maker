318ac19fdf728102f6932b7b7838bc62fb7f6d06
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-09-20 18:32:33 +0000

    added spacing to luchador create form

--- client/ui/LuchadorCreate.vue
+++ client/ui/LuchadorCreate.vue
@@ -1,30 +1,33 @@
 <template>
-    <div class='col-10 col-offset-1'>
+    <div class='col-10 offset-1'>
         <form>
             <div class='form-group'>
                 <label for='luchador-name'>Name of Fighter</label>
                 <input type='text' class='form-control' id='luchador-name'>
             </div>
-            <div class='form-group'>
-                <label for='luchador-height'>Height (In Inches)</label>
-                <input type='number' value='68' min='40' max='120' class='form-control' id='luchador-height'>
-            </div>
-            <div class='form-group'>
-                <label for='luchador-weight'>Weight (In Pounds)</label>
-                <input type='number' value='200' min='75' max='999' class='form-control' id='luchador-weight'>
+            <div class='row'>
+                <div class='form-group col-sm-5 offset-sm-1'>
+                    <label for='luchador-height'>Height (In Inches)</label>
+                    <input class='' type='number' value='68' min='40' max='120' class='form-control' id='luchador-height'>
+                </div>
+                
+                <div class='form-group col-sm-5 offset-sm-1'>
+                    <label for='luchador-weight'>Weight (In Pounds)</label>
+                    <input type='number' value='200' min='75' max='999' class='form-control' id='luchador-weight'>
+                </div>
             </div>
             <div class='form-group'>
                 <label for='luchador-mask-color'>Mask Color</label>
                 <select class="custom-select" id='luchador-mask-color'>
-                  <option selected>Choose Trim Color</option>
-                  <option v-for='color in colors' value={{index}}>{{color}}</option>
+                  <option selected>Choose Mask Color</option>
+                  <option v-for='color in colors' :value="color.index">{{color}}</option>
                   
                 </select></div>
             <div class='form-group'>
                 <label for='luchador-trim-color'>Trim Color</label>
                 <select class="custom-select" id='luchador-trim-color'>
                   <option selected>Choose Trim Color</option>
-                  <option v-for='color in colors' value={{index}}>{{color}}</option>
+                  <option v-for='color in colors' :value="color.index">{{color}}</option>
                   
                 </select>
             </div>
