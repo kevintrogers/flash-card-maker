40760e8464d3b189379debab5276c8e740d77af4
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-09-20 16:53:59 +0000

    added calculate Health function

--- client/ui/Scoreboard.vue
+++ client/ui/Scoreboard.vue
@@ -4,14 +4,14 @@
                 <span class='placard' id='player-placard'>YOU</span>
             </div>
             <div class='row'>
-                <div class='health-meter col-10 col-offset-1' id='player-health-meter'></div>
+                <div class='health-meter col-10 col-offset-1' id='player-health-meter' :style="{width:playerHealth * 8 + '%' }"></div>
             </div>
             
         <div class='row'>
                 <div class='placard' id='opponent-placard'><span>OPPONENT</span><img class='circle-img' src='http://media-cache-ak0.pinimg.com/736x/a0/b4/7b/a0b47bca16fb26652198608cf2135753.jpg'</span></div>
             </div>
             <div class='row'>
-                <div class='health-meter col-10 col-offset-1' id='opponent-health-meter'></div>
+                <div class='health-meter col-10 col-offset-1' id='opponent-health-meter' :style="{width:opponentHealth * 8 + '%' }"></div>
         </div>
         
         <div class='row'>
@@ -27,10 +27,26 @@ export default {
         return {
             
             questionCount: 0,
+            playerHealth:10,
+            opponentHealth:10,
+            playerWin: false,
+            opponentWin: false,
+        }
+    },
+            methods: {
+                calculateHealth: function() {
+                    if (this.playerWin) {
+                        this.opponentHealth -1;
+                    }
+                    if (this.OpponentWin) {
+                        this.playerHealth -1;
+                    }
+                    this.playerWin, this.OpponentWin = false;
+                }
             
-        };
-    }
-};
+            
+        }
+    };
     
 </script>
 <style>
