866073ac66121be3d4548dd6793355ea6cdb65e6
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-09-22 17:40:46 +0000

    moved business logic from luchador create to app.vue in order to faciliate data renderign across all components

--- client/ui/LuchadorCreate.vue
+++ client/ui/LuchadorCreate.vue
@@ -54,16 +54,6 @@
     </div>
 </template>
 <script>
-export default {
-    data() {
-        
-        return {
-            colors: ['orange','yellow','red','green','purple','blue','black','white'],
-            eyes: ['lightblue', 'lightgreen', 'blue', 'lightgrey', 'green', 'brown', 'black', 'red'],
-            styles: ['Classic', 'Beast', 'Chupacabra', 'Pretty Boy', 'Royal', 'Razzle Dazzle', 'Fabulous', 'Demon']
-        }
-        
-    }
-}
+
     
 </script>
