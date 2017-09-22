3917b3183298e979b91c9087eea70765725cc36b
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-09-22 16:52:00 +0000

    added luchador model component

---
+++ client/ui/LuchadorModel.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class='box'>
+    	<div class='head'>
+			<div class='mask'>
+				<div class='mask mask-left'></div>
+				<div class='mask mask-top'></div>
+				<div class='mask mask-right'></div>
+				<div class='ear-left'></div>
+				<div class='ear-right'></div>
+		<div class='lens-left-border'>
+				<div class='mask lens-left'></div>
+				</div>
+					<div class='eye eye-left'><div class='brow'></div><div class='iris'><div class='pupil'></div></div></div>
+					
+					<div class='single-tear-base'></div>
+					<div class='single-tear-drop'></div>
+				
+				<div class='lens-right-border'> 
+					<div class='mask lens-right'></div>	
+					</div>
+					<div class='eye eye-right'><div class='brow'></div><div class='iris'><div class='pupil'></div></div></div>
+		
+				<div class='glasses frame-right'></div>
+				<div class='nose'></div>
+				<div class='upper-lip'></div>
+				<div class='teeth'></div>
+				<div class='lower-lip'>
+					
+			</div>
+
+			<div class='singlet'></div>
+			<div class='neck'></div>
+				
+				<div class='shoulder-left'></div>
+				<div class='upper-arm-left'></div>
+				<div class='fore-arm-left'></div>
+				<div class='hand-left'></div>
+				
+				<div class='shoulder-right'></div>
+					<div class='upper-arm-right'></div>
+					<div class='fore-arm-right'></div>
+					<div class='hand-right'></div>
+					<div class='top-pack-left'></div><div class='top-pack-right'></div>
+					<div class='middle-pack-left'></div><div class='middle-pack-right'></div>
+					<div class='bottom-pack-left'></div><div class='bottom-pack-right'></div>
+					<div class='row belt'>
+					<div class='medallion valign-wrapper '><span class='center valign'>CHAMPION</span></div>
+					</div>
+					</div>			
+	</div>
+    
+</template>
+<script>
+    
+</script>
+<style>
+    @import url('scss/luchadorModel.scss');
+</style>
--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
         <luchador-create></luchador-create>
+        <luchador-model></luchador-model>
         <h1>Trivia Rumble</h1>
         <welcome class='welcome'></welcome>
         <scoreboard class='scoreboard'></scoreboard>
+        
         
         
     </div>
